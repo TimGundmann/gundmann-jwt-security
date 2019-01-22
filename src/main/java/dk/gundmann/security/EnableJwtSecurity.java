30de555f8fece435d1b0da3fc17abfce0cc44174
Tim Gundmann <tim@gundmann.dk>
2019-01-22 14:43:42 +0100

    security path updated

--- src/main/java/dk/gundmann/security/EnableJwtSecurity.java
+++
@@ -1,5 +0,0 @@
-package dk.gundmann.security;
-
-public @interface EnableJwtSecurity {
-
-}
--- src/main/java/dk/gundmann/security/WebAutoSecurityConfig.java
+++ src/main/java/dk/gundmann/security/WebAutoSecurityConfig.java
@@ -1,45 +1,25 @@
 package dk.gundmann.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 @EnableConfigurationProperties(SecurityProperties.class)
-@ConditionalOnClass(EnableJwtSecurity.class)
 @Order(101)
 public class WebAutoSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	private AuthenticationProvider authenticationProvider;
-
-	@Autowired
-	public WebAutoSecurityConfig(AuthenticationProvider authenticationProvider) {
-		this.authenticationProvider = authenticationProvider;
-	}
 	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
         	.authorizeRequests()
-                .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
             .and()
                 .addFilterBefore(new JWTAuthenticationFilter(),
                         UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider);
-    }
-    
 }
