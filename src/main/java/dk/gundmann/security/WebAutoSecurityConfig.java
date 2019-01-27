d038f3ea3a4ef41d5b27941b7947d5b0d11c39b6
Tim Gundmann <tim@gundmann.dk>
2019-01-27 14:07:48 +0100

    add cors filter

--- src/main/java/dk/gundmann/security/WebAutoSecurityConfig.java
+++ src/main/java/dk/gundmann/security/WebAutoSecurityConfig.java
@@ -1,5 +1,6 @@
 package dk.gundmann.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,15 +8,23 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.session.SessionManagementFilter;
 
 @Configuration
 @Order(101)
 @EnableWebSecurity
 public class WebAutoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    CorsFilter corsFilter() {
+        CorsFilter filter = new CorsFilter();
+        return filter;
+    }
+    
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
+			.addFilterBefore(corsFilter(), SessionManagementFilter.class) 
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 				.authorizeRequests().anyRequest().authenticated()
