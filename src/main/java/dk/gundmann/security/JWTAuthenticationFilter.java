09a45fbfdf9527a0ea1725c7e10761a69ed7421f
Tim Gundmann <tim@gundmann.dk>
2019-02-06 19:45:55 +0100

    method security

--- src/main/java/dk/gundmann/security/JWTAuthenticationFilter.java
+++ src/main/java/dk/gundmann/security/JWTAuthenticationFilter.java
@@ -3,7 +3,10 @@ package dk.gundmann.security;
 import static java.util.Collections.emptyList;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -13,6 +16,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -36,14 +41,14 @@ public class JWTAuthenticationFilter extends GenericFilterBean {
     private Authentication getAuthentication(HttpServletRequest request) {
         String token = request.getHeader(securityConfig.getHeaderString());
         if (token != null) {
-            return parseToken(token)
-            		.map(user -> new UsernamePasswordAuthenticationToken(user, null, emptyList()))
+            return parseUserName(token)
+            		.map(user -> new UsernamePasswordAuthenticationToken(user, null, parseRoles(token)))
             		.orElse(null);
         }
         return null;
     }
 
-	private Optional<String> parseToken(String token) {
+	private Optional<String> parseUserName(String token) {
 		return Optional.ofNullable(Jwts.parser()
 		        .setSigningKey(securityConfig.getSecret())
 		        .parseClaimsJws(token.replace(securityConfig.getTokenPrefix(), ""))
@@ -51,4 +56,16 @@ public class JWTAuthenticationFilter extends GenericFilterBean {
 		        .getSubject());
 	}
 
+	private Collection<? extends GrantedAuthority> parseRoles(String token) {
+		return Arrays.asList(Jwts.parser()
+		        .setSigningKey(securityConfig.getSecret())
+		        .parseClaimsJws(token.replace(securityConfig.getTokenPrefix(), ""))
+		        .getBody()
+		        .get("roles").toString().split(",")).stream()
+				.filter(r -> !"".equals(r))
+                .map(authority -> {
+                	return new SimpleGrantedAuthority(authority);
+                })
+                .collect(Collectors.toList());
+	}
 }
--- src/main/java/dk/gundmann/security/WebAutoSecurityConfig.java
+++ src/main/java/dk/gundmann/security/WebAutoSecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 @EnableConfigurationProperties(SecurityConfig.class)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebAutoSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private SecurityConfig securityConfig;
