86ec05f200c67ce52d194427220022586759102f
Tim Gundmann <tim@gundmann.dk>
2025-04-27 19:47:29 +0200

    move auto config

--- src/main/java/dk/gundmann/security/WebAutoSecurityConfig.java
+++ src/main/java/dk/gundmann/security/WebAutoSecurityConfig.java
@@ -1,13 +1,8 @@
 package dk.gundmann.security;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +13,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @AutoConfiguration
 @EnableWebSecurity
