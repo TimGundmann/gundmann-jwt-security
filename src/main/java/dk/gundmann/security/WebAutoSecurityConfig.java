edeccbbe45b49a39f19012cf3afc730fbd9299e7
Tim Gundmann <tim@gundmann.dk>
2025-04-27 19:47:16 +0200

    move auto config

--- src/main/java/dk/gundmann/security/WebAutoSecurityConfig.java
+++ src/main/java/dk/gundmann/security/WebAutoSecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +19,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
+@AutoConfiguration
 @EnableWebSecurity
 @EnableConfigurationProperties(SecurityConfig.class)
 @EnableMethodSecurity(prePostEnabled = true)
